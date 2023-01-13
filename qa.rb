310d7471d8fc8984f593c155af0fecbc4979756b
Shubham <shubhamveer228@gmail.com>
2023-01-13 13:04:26 +0530

    added trigger code

---
+++ qa.rb
@@ -0,0 +1,12 @@
+class Qa < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/shubh018/qa-cmd/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "8da02d2ce3db8096fb75c74698fdd275b32d29692b83485f8b217224469b4a59"
+  license ""
+
+  def install
+	bin.install "qa"
+  end
+
+end
