b5b04f3aec73749a98fbbc45702bfa8c8700369b
Shubham <shubhamveer228@gmail.com>
2023-01-14 22:45:02 +0530

    version upgraded to v1.0.3

--- qa.rb
+++ qa.rb
@@ -1,12 +1,11 @@
 class Qa < Formula
   desc ""
   homepage ""
-  url "https://github.com/shubh018/qa-cmd/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "8304c263773b3a9d145ae62ee7330a10bd78438b8e47c88c4b86c969745d5025"
+  url "https://github.com/shubh018/qa-cmd/archive/refs/tags/v1.0.3.tar.gz"
+  sha256 "38eae01a34db183c601009282a5e86b229bf42e4f7565c2b25dd21469ad893e8"
   license ""
 
   def install
 	bin.install "qa"
   end
-
 end
